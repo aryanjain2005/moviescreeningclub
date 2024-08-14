4dcc5f89399bd2ae1d385f9c1ec8ce52e780ba83
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2024-08-15 03:32:32 +0530

    fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const baseUrl =
   import.meta.env.VITE_environment === 'development'
-    ? 'http://chalchitra.iitmandi.ac.in/api'
+    ? 'https://chalchitra.iitmandi.ac.in/api'
     : '/api'
 export const api = axios.create({
   baseURL: baseUrl,
